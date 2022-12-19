bf48a4004f218ca386815f59bc3bf27497818dc4
Leonid Rublevskiy <leops@inbox.ru>
2022-12-19 14:28:40 +0300

    delete unnecessary comments

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,3 @@
-/* -- В дальнейшем сюда переместить все константы и переменные, чтобы визуально разгрузить папку "index.js"  */
 
 // Делаем выборку ДОМ элементов:
 //  Глобальные переменные:
@@ -7,13 +6,6 @@
 export const modalIsOpen = document.querySelector('.popup_opened');//открытый попап
 export const popups = document.querySelectorAll('.popup');//* здесь назначили переменную, которая работает со всеми блоками '.popup'
 export const popupCloseBtnElements = document.querySelectorAll('.popup__close-btn');//* здесь назначили все кнопки закрытия попАпов
-
-//export const profileCloseBtn = document.querySelector('.popup__close-btn_profile');// кнопка закрытия редактора профиля
-
-//export const cadrAddCloseBtn = document.querySelector('.popup__close-btn_card');// кнопка закрытия
-
-//export const zoomCloseBtn = document.querySelector('.popup__close-btn_zoom');// кнопка закрытия
-
 
 export const popuProfileEdit = document.querySelector('.popup_profile');//* эта переменная добавляет класс открытия попапу профиля при клике
 export const popupNewCardAdd = document.querySelector('.popup_new-card');//* эта переменная добавляет класс открытия попапу новой карточки при клике
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
 /* Здравствуйте, Павел! Ещё раз спасибо Вам за Вашу работу, она очень важна для таких новичков, как я!
-  Отдельное Вам спасибо за подробное объяснение по работе функций! Я бы с удовольствием у Вас учился! Даже в тренажёре не было рассказано о таких тонкостях. А Ваша подсказка с функцией закрытия по-клику в оверлей или по-крестику - это ж на мой взгляд гениально!  Спасибо Вам. Спасибо!!!
-Спасибо. С уважением, Леонид.
 
-По-поводу названий функций- у меня явно не хватает знаний английского. (стандартный базовый школьный курс, который у меня был 28лет назад) Видимо, придётся изучать еще и язык...
+Подправил, вроде всё работает теперь, почти как раньше. 
+Единственное, кнопка при открытии редактора профиля в попап - неактивна, 
+если не менялись поля. Не знаю пока, как правильно. В задании, вроде, нет акцента на этом моменте, а как по-мне: если ничего не менял, то и пересохранять необходимости нет. (если я не прав, то, конечно же, подправлю с удовольствием).
 
-Извиняюсь за обилие коментариев. Я знаю, что их нужно минимизировать, но пока
-я их оставляю больше для себя, в качестве подсказок. Если они мешают проверке - конечно я всё сохраню в отдельном файле , а тут всё почищу. Только скажите)) */
+Чуть почистил от коментариев.
+
+Спасибо. С уважением, Леонид.*/
 
 import {popups} from './constants.js';
-//import {profileCloseBtn} from './constants.js';
-//import {cadrAddCloseBtn} from './constants.js';
-//import {zoomCloseBtn} from './constants.js';
 import {popuProfileEdit} from './constants.js';
 import {popupNewCardAdd} from './constants.js';
 import {popuProfile} from './constants.js';
@@ -39,7 +37,6 @@ import { click } from './constants.js';
 //========================================================
 
 enableValidation(validationObj);
-/*enableValidation должна вызываться один раз в глобальной области видимости. Сейчас получается, что при открытии форм каждый раз заново запускаются все функции из модуля валидации — заново выбираются по селектору все необходимые элементы на странице, заново устанавливаются все слушатели, переназначаются все обработчики. Это относительно ресурсоемкий процесс, негативно сказывающийся на производительности.*/
 
 //---  Добавляем карточки
 const generateCard = (dataCard) => {
@@ -114,19 +111,10 @@ const handleSubmitFormAddCard = (evt) => {
 };
 
 // Реализуем работу не через "переключатель", а через разные функции:
-//* openPopup -это общая функция открытия любого попАпа. "evt" мы назначаем сами, это может быть "е" или любое другое имя, указывается для того, чтобы функция "видела" какой из элементов на странице её вызвал.
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
   document.addEventListener('keydown', closePopupByEsc);
 };
-
-//* closePopup принимает на вход любую кнопку (на закрытие) и методом "forEach" перебирает весь массив "popups" и в каждом елементе удаляет(remove) модификатор ('popup_opened')(без точки т.к. это именно класс а не селектор)
-/*
-const closePopup = () => {
-  popups.forEach(function(element){
-    element.classList.remove('popup_opened');
-  })
-};*/
 
 const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
@@ -134,7 +122,7 @@ const closePopup = (popup) => {
 }
 
 
-//* функция "слушает" конкретную кнопку(методом addEventListener), в данном случае кнопку редактирования профиля и при клике вызывает функцию "openPopup",передавая ей параметром "(popuProfileEdit)", элемент в котором отработает функция., а "(evt)" указывает конкретную кнопку.
+//* функция "слушает" конкретную кнопку(методом addEventListener)
 popupProfileButtonOpen.addEventListener(click, () => {
   //console.log('klick');
   fillingProfile();
@@ -155,38 +143,6 @@ const closePopupByClick = (e) => {
     closePopup(e.currentTarget);
   }
 };
-
-/*const closePopupByEscBtn = (evt) => {
-  const modalIsOpen = document.querySelector('.popup_opened');
-  if (evt.key === escButton) {
-    closePopup(modalIsOpen);
-  }
-}
-*/
-
-/* закрытие всех попапов я решил организовать при помощи метода forEach:  мы "слушаем" клик на всех кнопках закрытия любого попАпа
-
-popupCloseBtnElements.forEach( (evt) => evt.addEventListener(click, closePopup) );
-*/
-
-/*/-- Зеркальное закрытие попапов ==============================
-Необходимость в этих функциях отпала, т.к. это выполняется в функции closePopupByClick
-
-profileCloseBtn.addEventListener(click, () => closePopup(popuProfileEdit));
-
-cadrAddCloseBtn.addEventListener(click, () => closePopup(popupNewCardAdd));
-
-zoomCloseBtn.addEventListener(click, () => closePopup(zoomPopup));
-
-*///==============================================================
-
-
-//* закрытие любого попапа по клавише "ESC"
-/*
-document.addEventListener('keydown', function(evt) {
-  if (evt.key === escButton) closePopup();
-});
-*/
 
 const closePopupByEsc = (e) => {
   if (e.key === escButton) {
