65a568586cb0835277a51f9ba7eacd9bf5d0b0dd
Leonid Rublevskiy <leops@inbox.ru>
2023-01-30 07:03:29 +0300

    replace indexJS

--- scripts/index.js
+++ scripts/index.js
@@ -1,153 +1,136 @@
-
 //*-- Импорты:
 import{
   // - элементы ДОМ
-  popups,
-  popuProfileEdit,
-  popupNewCardAdd,
-  popuProfileName,
-  popuProfileActivity,
   popupProfileButtonOpen,
   popupCardButtonOpen,
   formProfileEdit,
-  formNameInput,
-  formAboutInput,
   formAddCard,
   formPlaceNameInput,
   formPlaceLinkInput,
-  zoomPopup,
-  imgPopupZoom,
-  imgTitlePopupZoom,
   // - элемент для карточек
   cardsContainer,
   // - кнопки
-  escButton,
   CLICK
-} from './constants.js';
+} from '../utils/constants.js';
 
+import Section from '../components/Section.js';
 //*--
-import {initialCards} from './constants.js';
+import {initialCards} from '../utils/constants.js';
 //*--
-import {validationObj} from './constants.js';
+import {validationObj} from '../utils/constants.js';
 //*--
-import { FormValidator } from './FormValidator.js';
-import { Card } from './Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Card } from '../components/Card.js';
+
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
+//
+//import {createCard} from '../utils/utils.js';
 
 //========================================================
 
-//*-- Функция создания новой валидации
-const createValidator = (formSelector) => {
-  const validator = new FormValidator (validationObj, formSelector);
-  return validator;
-};
+const cardAdd = new Section({
+  items: initialCards,
+  renderer: (item) =>{
+    const card = new Card(item, '#card-template', handleImageClick);
 
-//*-- Валидация формы редактирования профиля
-const formProfileEditValidator = createValidator(formProfileEdit);
-formProfileEditValidator.enableValidation();
+      const cardElement = card.generateCard();
+      cardAdd.addItem(cardElement);
+  },
+}, cardsContainer);
 
-
-//*-- Валидация формы добавления карточки
-const addCardFormValidator = createValidator(formAddCard);
-addCardFormValidator.enableValidation();
-
-//*-- Функция открытия попапа с картинкой
-const handleImageClick = (nameAdd, imageAdd) => {
-  imgPopupZoom.src = imageAdd;
-  imgPopupZoom.alt = nameAdd;
-  imgTitlePopupZoom.textContent = nameAdd;
-  openPopup(zoomPopup);
-};
-
-// Функция создания карточки
-const createCard = (item) =>{
-  const card = new Card(item, '#card-template', handleImageClick);
-  const cardElement = card.generateCard();
-  return cardElement;
-};
-initialCards.forEach((item) => {
-  cardsContainer.append(createCard(item));
+const newCardAdd = new Section({
+  renderer: (item) =>{
+    const card = new Card(item, '#card-template', handleImageClick);
+    const cardElement = card.generateCard();
+    cardAdd.addNewCard(cardElement);
+    //newCardAdd.setEventListeners();
+  }
 });
 
-//* Функция добавления карточки через форму ("Submit")
-const addNewCard = (item) => {
-  //const element = generateCard(dataCard);
-  cardsContainer.prepend(createCard(item));
+const checkValidNewCard = new FormValidator (validationObj, formAddCard);
+checkValidNewCard.enableValidation();
+const handleAddCard = new PopupWithForm('.popup_new-card', handleSubmitFormAddCard);
+handleAddCard.setEventListeners();
+
+const checkValidProfile = new FormValidator (validationObj, formProfileEdit);
+checkValidProfile.enableValidation();
+const profileEdit = new PopupWithForm('.popup_profile',(handleEditFormProfile));
+profileEdit.setEventListeners();
+const userInfo = new UserInfo({nameSelector: '.profile__user-name', activSelector: '.profile__user-activity'});
+
+const newPopupWithImage = new PopupWithImage('.popup_zoom');
+newPopupWithImage.setEventListeners();
+
+
+//*-- Функция открытия попапа с картинкой
+export const handleImageClick = (nameAdd, imageAdd) => {
+  newPopupWithImage.open(nameAdd, imageAdd);
 };
 
-//* Функция заполнения полей "инпут"
+/* Функция заполнения полей "инпут"
 const fillProfile = () =>{
   formNameInput.value = popuProfileName.textContent;
   formAboutInput.value = popuProfileActivity.textContent;
-};
+};*/
 
 //*  функция сохранения (отправки) введённых данных для сохранения новых значений в попапе редактора профиля
-const handleSubmitFormProfile = (evt) => {
-  evt.preventDefault();
-  const cardData = {
-    name: formPlaceNameInput.value,
-    link: formPlaceLinkInput.value
-  };
-  addNewCard(cardData);
-  closePopup(popupNewCardAdd);
-  evt.target.reset();
+function handleSubmitFormAddCard(e, data) {
+  e.preventDefault();
+  newCardAdd.renderer(data);
+
 };
 
-//*  Функция сохранения (отправки) введённых данных для добавления карточки
-const handleSubmitFormAddCard = (evt) => {
-  evt.preventDefault();//* метод присваивает выбранные значения
-  popuProfileName.textContent = formNameInput.value;
-  popuProfileActivity.textContent = formAboutInput.value;
-  closePopup(popuProfileEdit);
-};
-
-// Реализуем работу не через "переключатель", а через разные функции:
-const openPopup = (popup) => {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePopupByEsc);
-};
-const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupByEsc);
+//*  Функция сохранения (отправки) введённых данных редактора профиля
+function handleEditFormProfile (e, values) {
+  e.preventDefault();//* метод присваивает выбранные значения
+  userInfo.setUserInfo(values.name, values.activity);
+  console.log('!????!!');
+  profileEdit.close();
 };
 
 // Функция открытия попапа редактора профиля
 const openPopupProfile = () => {
-  fillProfile();
-  //resetErrs(popuProfile, validationObj);
-  formProfileEditValidator.resetErrs();
-  formProfileEditValidator.handleBtnCheckValidity();
-  openPopup(popuProfileEdit);
+  console.log('//ниже открываем "profileEdit"');
+  const {name, activity} = userInfo.getUserInfo();
+  profileEdit.setFormVal({name, activity});
+  console.log(name);
+  console.log(activity);
+  checkValidProfile.resetErrs();
+
+  profileEdit.open();
 };
 
 // Функция открытия попапа добавления карточки
 const openPopupAddNewCard = () =>{
-  openPopup(popupNewCardAdd);
+  console.log('//ниже открываем "handleAddCard"');
+  console.log(handleAddCard);
+  checkValidNewCard.resetErrs();
+  handleAddCard.open();
 };
 
-// Слушатель кнопки редактора профиля
+// Назначаем слушатель на кнопку редактора профиля
 popupProfileButtonOpen.addEventListener(CLICK, openPopupProfile);
 
-// Слушатель кнопки новой карточки
+// Назначаем слушатель на кнопку добавления карточки
 popupCardButtonOpen.addEventListener(CLICK, openPopupAddNewCard);
 
-//* -- Функция закрытия попапа при клике в крестик или оверлей
-const closePopupByClick = (e) => {
-  if (e.target === e.currentTarget || e.target.classList.contains('popup__close-btn')) {
-    closePopup(e.currentTarget);
-  }
-};
-
-//* -- Функция закрытия попапа клавишей ESC
-const closePopupByEsc = (e) => {
-  if (e.key === escButton) {
-    closePopup(document.querySelector('.popup_opened'))
-  }
-};
-
-popups.forEach((popup) => popup.addEventListener(CLICK, closePopupByClick));
-
-formAddCard.addEventListener('submit', handleSubmitFormProfile);//* слушаем кнопку "создать" в попапе редактора профиля. При нажатии (событие'submit')выполнить функцию "handleSubmitFormProfile"
-
-formProfileEdit.addEventListener('submit', handleSubmitFormAddCard);//* слушаем кнопку "сохранить" в попапе редактора профиля. При нажатии ('submit')выполнить функцию "formProfileSubmitHandler"
+cardAdd.renderItems();
+/*
+const debuging = (e) => {
+  e.preventDefault();
+  const cardData = {
+    name: formPlaceNameInput.value,
+    link: formPlaceLinkInput.value
+  };
+  //console.log(cardData);
+  handleAddCard.close();
+  newCardAdd.renderer(cardData);
+  handleAddCard.resetErrs();
+}
 
   //console.log('end index js');
+  formAddCard.addEventListener('submit', debuging);//* слушаем кнопку "создать" в попапе редактора профиля. При нажатии (событие'submit')выполнить функцию "handleSubmitFormProfile"
+
+  formProfileEdit.addEventListener('submit', handleSubmitFormAddCard);//* слушаем кнопку "сохранить" в попапе редактора профиля. При нажатии ('submit')выполнить функцию "formProfileSubmitHandler"*/
